209f8b2de83474df62136234f8b7be8bb7d095b7
apnitor-developer <developer@apnitor.com>
2025-12-22 18:36:34 +0530

    Remove unique constraint from the Role Table

--- src/main/java/com/example/account/receivable/User/entity/Role.java
+++ src/main/java/com/example/account/receivable/User/entity/Role.java
@@ -20,7 +20,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 50)
+    @Column(nullable = false, length = 50)
     private String name;
 
     @Column(length = 255)
