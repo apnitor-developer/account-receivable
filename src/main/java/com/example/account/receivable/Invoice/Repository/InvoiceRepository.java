b90d6fb54617dd61be0ba00a84a2bf41c0b3dbde
apnitor-developer <developer@apnitor.com>
2026-01-13 18:03:51 +0530

    not show the Written_off and paid invoices

--- src/main/java/com/example/account/receivable/Invoice/Repository/InvoiceRepository.java
+++ src/main/java/com/example/account/receivable/Invoice/Repository/InvoiceRepository.java
@@ -15,7 +15,7 @@ import com.example.account.receivable.Invoice.Entity.Invoice;
 public interface InvoiceRepository extends JpaRepository<Invoice , Long> {
     Page<Invoice> findByDeletedFalse(Pageable pageable);
 
-    List<Invoice> findByCustomerIdAndDeletedFalse(Long customerId , String status);
+    List<Invoice> findByCustomerIdAndDeletedFalseAndStatusNotIn(Long customerId , List<String> statuses);
 
     List<Invoice> findByCustomerId(Long customerId);
 
--- src/main/java/com/example/account/receivable/Invoice/Service/InvoiceService.java
+++ src/main/java/com/example/account/receivable/Invoice/Service/InvoiceService.java
@@ -349,7 +349,7 @@ public class InvoiceService {
 
     // Get Single customer invoice
     public List<Invoice> getSingleCustomerInvoice(Long customerId) {
-        return invoiceRepository.findByCustomerIdAndDeletedFalse(customerId , "WRITTEN_OFF");
+        return invoiceRepository.findByCustomerIdAndDeletedFalseAndStatusNotIn(customerId , List.of("WRITTEN_OFF", "PAID"));
     }
 
     // Invoice By Id
