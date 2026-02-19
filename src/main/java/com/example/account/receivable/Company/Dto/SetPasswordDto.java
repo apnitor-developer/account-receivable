a2971934db22e0567e6b01489f6d3e888ef31af3
apnitor-developer <developer@apnitor.com>
2026-02-19 12:31:51 +0530

    Apply Password Validation on the Set-Password

--- src/main/java/com/example/account/receivable/Company/Dto/SetPasswordDto.java
+++ src/main/java/com/example/account/receivable/Company/Dto/SetPasswordDto.java
@@ -1,9 +1,19 @@
 package com.example.account.receivable.Company.Dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class SetPasswordDto {
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 12, message = "Password must be at least 12 characters")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-={}\\[\\]:;\"'<>,.?/]).{12,}$",
+        message = "Password must contain uppercase, lowercase and special character"
+    )
     private String password;
 }
