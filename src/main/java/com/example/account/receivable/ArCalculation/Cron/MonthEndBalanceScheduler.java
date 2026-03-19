5c6d03a3d5ce1c49eda5cf1b986384855d9cfea8
Narinder Dev <narinder.dev@gmail.com>
2026-03-19 13:43:23 +0530

    Change Cron job time now it runs on 1st day of every month at 12:01

--- src/main/java/com/example/account/receivable/ArCalculation/Cron/MonthEndBalanceScheduler.java
+++ src/main/java/com/example/account/receivable/ArCalculation/Cron/MonthEndBalanceScheduler.java
@@ -3,12 +3,16 @@ package com.example.account.receivable.ArCalculation.Cron;
 import com.example.account.receivable.ArCalculation.Service.CompanyMonthEndSnapshotService;
 import com.example.account.receivable.Company.Repository.CompanyRepository; // adjust to your actual path
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MonthEndBalanceScheduler {
@@ -17,8 +21,9 @@ public class MonthEndBalanceScheduler {
     private final CompanyRepository companyRepository;
 
     // Runs on 1st day of every month at 00:10 (India time)
-    @Scheduled(cron = "0 2 8 * * *")
+    @Scheduled(cron = "0 1 0 1 * *")
     public void runMonthlySnapshot() {
+        log.info("🔥 Scheduler triggered at {}", LocalDateTime.now());
 
         YearMonth targetMonth = YearMonth.now().minusMonths(1);
 
